a6ddc57ae9e25d49a1bdebcdc8a4ccf28b912493
Crafty Jumper <blueghostbear@gmail.com>
2026-03-17 10:06:36 -0700

    In case you havent figured it out, your choices don't matter

--- textbox.gd
+++ textbox.gd
@@ -22,16 +22,44 @@ const audibleLetters:Array[String] = [
 	".",",","!","?"
 ]
 var choosing: bool = false
-
+var choice: int = 0
 @onready var choices_node: Node2D = $Choices
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var _1: RichTextLabel = $"Choices/1"
+@onready var _2: RichTextLabel = $"Choices/2"
+@onready var _3: RichTextLabel = $"Choices/3"
+@onready var _4: RichTextLabel = $"Choices/4"
+var writingChoice: int = 0
+var choiceName: String = ""
+
 func _process(_delta: float) -> void:
 	
 	if Input.is_action_just_pressed("c"):
 		get_textbox(textboxId)
 		
+	if choices_node.visible:
+		if Input.is_action_just_pressed("left"):
+			choice = 1
+		if Input.is_action_just_pressed("right"):
+			choice = 2
+		if Input.is_action_just_pressed("up"):
+			choice = 3
+		if Input.is_action_just_pressed("down"):
+			choice = 4
 	
+	_1.modulate = Color.WHITE
+	_2.modulate = Color.WHITE
+	_3.modulate = Color.WHITE
+	_4.modulate = Color.WHITE
+	
+	if choice == 1:
+		_1.modulate = Color.YELLOW
+	if choice == 2:
+		_2.modulate = Color.YELLOW
+	if choice == 3:
+		_3.modulate = Color.YELLOW
+	if choice == 4:
+		_4.modulate = Color.YELLOW
 	
 	frameCounter += 1
 	if waitForInp:
@@ -48,8 +76,10 @@ func _process(_delta: float) -> void:
 	else:
 		if remainMsg == "":
 			get_textbox_sub(textboxId)
-	
-	addLetter()
+	if choosing:
+		write_to_choice()
+	else:
+		addLetter()
 
 func get_textbox_sub(id:int=0):
 	tweenSubplot()
@@ -61,11 +91,55 @@ func get_textbox_sub(id:int=0):
 	if FileAccess.file_exists("res://textbox/" + textbox[id]["speakers"][msgIdx] + "/" + str(int(textbox[id]["faces"][msgIdx])) + ".png"):
 		subplot.show()
 		subplot.texture = load("res://textbox/" + textbox[id]["speakers"][msgIdx] + "/" + str(int(textbox[id]["faces"][msgIdx])) + ".png")
+
+func write_to_choice() -> void:
+	if fmod(frameCounter,2) == 0:
+		return
+	if choiceName == "":
+		if writingChoice > 0:
+			if textboxSet[msgIdx].get(writingChoice-1) is String:
+				choiceName = textboxSet[msgIdx].get(writingChoice-1)
+	var currNode = rich_text_label
+	if writingChoice == 1:
+		currNode = _1
+	if writingChoice == 2:
+		currNode = _2
+	if writingChoice == 3:
+		currNode = _3
+	if writingChoice == 4:
+		currNode = _4
 	
+	var character = choiceName.substr(0,1)
+	currNode.text += character
+	choiceName = choiceName.erase(0)
+	
+	if audibleLetters.has(character):
+		var path = "res://textbox/sounds/snd_" + textbox[textboxId]["talksounds"][msgIdx] + ".wav"
+		if FileAccess.file_exists(path):
+			audio_stream_player.stream.set_stream(0,load(path))
+			for i in 10:
+				audio_stream_player.stream.set_stream_probability_weight(i,0)
+			audio_stream_player.stream.set_stream_probability_weight(0,1)
+		else:
+			load_randomized_sounds()
+		audio_stream_player.play()
+	if choiceName == "":
+		if writingChoice < textboxSet[msgIdx].size() and writingChoice != 0:
+			writingChoice += 1
+		else:
+			writingChoice = 0
+			choosing = false
 
 func show_choices() -> void:
+	choice = 0
+	writingChoice = 1
+	_1.text = ""
+	_2.text = ""
+	_3.text = ""
+	_4.text = ""
 	remainMsg = "/"
 	show()
+	choosing = true
 	rich_text_label.hide()
 	choices_node.show()
 	sprite_2d.hide()
